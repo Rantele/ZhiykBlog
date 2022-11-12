ec7a7079f8065b265e120edb312fa6af041074b4
rantele <rantele@163.com>
2022-11-12 12:39:14 +0800

    Complete the search function and add skeleton style

--- src/views/userCenter/userBlog/userBlog.vue
+++ src/views/userCenter/userBlog/userBlog.vue
@@ -4,19 +4,39 @@
     <el-card>
       <template #header>
         <div class="header">
-          <span>文章管理</span>
-          <el-input class="search" placeholder="请输入标题关键字">
+          <span>文章管理({{ dataList.length }})</span>
+          <el-input class="search" v-model="searchVal" placeholder="请输入标题关键字" @keyup.enter="handleSearch">
             <template #suffix>
-              <el-link :underline="false">
-                <el-icon class="el-input__icon">
-                  <IEpsearch />
+              <el-button link @click="handleSearch">
+                <el-icon :size="16">
+                  <Search />
                 </el-icon>
-              </el-link>
+              </el-button>
             </template>
           </el-input>
         </div>
       </template>
-      <BlogItem v-for="(item,index) in dataList" :index="index" :key="item.title" :data="item" @delete="deleteBlog" />
+      <el-skeleton :loading="loading" animated>
+        <template #template>
+          <div style="padding:10px 20px">
+            <el-skeleton-item variant="text" style="width: 40%;margin-bottom: 12px;" />
+            <el-skeleton-item variant="text" style="width: 100%;margin-bottom: 12px;" />
+            <el-skeleton-item variant="text" style="width: 80%;margin-bottom: 12px;" />
+            <el-skeleton-item variant="text" style="width: 60%" />
+          </div>
+
+        </template>
+        <template #default>
+          <template v-if="dataList.length > 0">
+            <BlogItem v-for="(item, index) in dataList" :index="index" :key="item.title" :data="item"
+              @delete="deleteBlog" />
+          </template>
+          <template v-else>
+            <el-empty />
+          </template>
+        </template>
+      </el-skeleton>
+
     </el-card>
   </div>
   <el-dialog v-model="deleteDialogVisible" title="删除文章" width="30%" :before-close="handleDeleteDialogClose">
@@ -33,7 +53,7 @@
 </template>
 
 <script lang='ts' setup>
-import { reactive, toRefs, onMounted } from 'vue'
+import { reactive, toRefs, onMounted, watch } from 'vue'
 import BlogItem from './components/BlogItem.vue'
 import { getUserMdList, deleteMd } from '@/request/api'
 import { ElMessage } from 'element-plus';
@@ -46,7 +66,9 @@ const state = reactive<{
     id: number;
     blogid: number;
     index: number;
-  }
+  };
+  loading: boolean;
+  searchVal: string;
 }>({
   dataList: [],
   deleteDialogVisible: false,
@@ -54,19 +76,26 @@ const state = reactive<{
     id: -1,
     blogid: -1,
     index: -1,
-  }
+  },
+  loading: true,
+  searchVal: ''
 })
 
-const { dataList, deleteDialogVisible, deleteData } = toRefs(state)
+const { dataList, deleteDialogVisible, deleteData, loading, searchVal } = toRefs(state)
 
-onMounted(() => {
-  getUserMdList().then((res) => {
+onMounted(async () => {
+  //init->SearchVal
+  // searchVal.value = '';
+  await getUserMdList().then((res) => {
     if (res.code === 200) {
       dataList.value = res.data
     }
   }).catch(err => {
     console.log('[catch]:', err);
   })
+  setTimeout(() => {
+    loading.value = false;
+  }, 500)
 })
 
 //点击删除按钮
@@ -75,7 +104,6 @@ const deleteBlog = (id: number, blogid: number, index: number) => {
   deleteData.value.id = id;
   deleteData.value.blogid = blogid;
   deleteData.value.index = index;
-  console.log(id, blogid);
 }
 
 //关闭删除提示框
@@ -99,6 +127,21 @@ const handleDeleteConfirm = () => {
   })
   deleteDialogVisible.value = false;
 
+}
+
+//搜索文章操作
+const handleSearch = async () => {
+  loading.value = true;
+  await getUserMdList({ search: searchVal.value }).then((res) => {
+    if (res.code === 200) {
+      dataList.value = res.data
+    }
+  }).catch(err => {
+    console.log('[catch]:', err);
+  })
+  setTimeout(() => {
+    loading.value = false;
+  }, 500)
 }
 </script>
 <style lang='less' scoped>
